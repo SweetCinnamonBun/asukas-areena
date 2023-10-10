932e5fd593a2d202eb2cb430e2bc833326a49d7d
roman.klemiato <roman.klemiato18@gmail.com>
2023-10-10 14:46:39 +0300

    changes to booking section

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,39 +1,82 @@
-// const date = new Date();
-// const currentMonth = document.getElementById("current-month");
-// const chevronLeft = document.getElementById("chevron-left");
-// const chevronRight = document.getElementById("chevron-right");
-// const calendarGrid = document.querySelector(".calendar-grid");
+const date = new Date();
 
+const renderCalendar = () => {
+  date.setDate(1);
 
+  const monthDays = document.querySelector(".days");
 
-// const months = [
-//     "Tammikuu",
-//     "Helmikuu",
-//     "Maaliskuu",
-//     "Huhtikuu",
-//     "Toukokuu",
-//     "Kesäkuu",
-//     "Heinäkuu",
-//     "Elokuu",
-//     "Syyskuu",
-//     "Lokakuu",
-//     "Marraskuu",
-//     "Joulukuu"
-// ]
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
 
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate();
 
+  const firstDayIndex = date.getDay();
 
-// chevronLeft.addEventListener("click", function() {
-//     date.setMonth(date.getMonth() - 1);
-//     currentMonth.textContent = months[date.getMonth()];
-//     console.log(date.getMonth());
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
 
-// })
+  const nextDays = 7 - lastDayIndex - 1;
 
-// chevronRight.addEventListener("click", function() {
-//     date.setMonth(date.getMonth() + 1)
-//     currentMonth.textContent = months[date.getMonth()];
-//     console.log(date.getMonth());
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-// })
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
 
+  document.querySelector(".date p").innerHTML = new Date().toDateString();
+
+  let days = "";
+
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDay; i++) {
+    if (
+      i === new Date().getDate() &&
+      date.getMonth() === new Date().getMonth()
+    ) {
+      days += `<div class="today">${i}</div>`;
+    } else {
+      days += `<div>${i}</div>`;
+    }
+  }
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`;
+    monthDays.innerHTML = days;
+  }
+};
+
+document.querySelector(".prev").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+});
+
+renderCalendar();
